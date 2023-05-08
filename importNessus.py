d21b5ccb3f056dc24cdec83c47aee109dc704ea5
TechnoSavage <technosavage@protonmail.com>
2023-05-08 16:19:49 -0400

    revised to pull variables from .env file.

--- importNessus.py
+++ importNessus.py
@@ -1,8 +1,9 @@
 """ EXAMPLE PYTHON SCRIPT! NOT INTENDED FOR PRODUCTION USE! 
-    importNessus.py, version 2.0 by Derek Burke
+    importNessus.py, version 3.0 by Derek Burke
     Bulk import all .nessus files in a specified folder via the runZero API."""
 
 import json
+import os
 import re
 import requests
 import subprocess
@@ -20,49 +21,19 @@ def usage():
                     API key for which the site belongs to. This script should work on *nix systems
                     and MacOS.
 
-                    You will be prompted to provide your runZero Organization API key
+                    You will be prompted to provide your runZero Organization API key if not specified
+                    in the .env file.
                     
                     Optional arguments:
 
                     -u <uri>              URI of console (default is https://console.runzero.com)
                     -s <site ID>          Site ID to apply scan data to
-                    -d <path/directory>   Specify directory path to import .nessus scans from
-                    -c <config file/path> Filename of config file including absolute path.
-                    -g                    Generate config file template.
+                    -d <path/directory>   Specify directory path to .nessus scan files
                     -h                    Show this help dialogue
                     
                 Examples:
                     importNessus.py -s 92hu9740-1ed3-4365-b6a4-733638f2a63d
-                    importNessus.py -u https://custom.runzero.com -s 92hu9740-1ed3-4365-b6a4-733638f2a63d -d /documents/scans/
-                    python -m importNessus -c example.config""")
-    
-def genConfig():
-    """Create a template for script configuration file."""
-
-    template = "orgToken= #Organization API Key\nuri=https://console.runzero.com #Console URL\nsiteID= #ID of site to upload nessus scan to\nnessusDir= #Directory path to nessus scan files\n"
-    writeFile("config_template", template)
-    exit()
-
-def readConfig(configFile):
-    """ Read values from configuration file
-
-        :param config: a file, file containing values for script
-        :returns: a tuple, console url at index 0, API token at index 1, site ID at index 2, and nessus file directory at index 3.
-        :raises: IOError: if file cannot be read.
-        :raises: FileNotFoundError: if file doesn't exist."""
-    try:
-        with open( configFile, 'r') as c:
-            config = c.read()
-            url = re.search("uri=(http[s]?://[a-z0-9.]+)", config).group(1)
-            token = re.search("orgToken=([0-9A-Z]+)", config).group(1)
-            siteID = re.search("siteID=([0-9a-z-]+)", config).group(1)
-            #nessusDir regex may need to be modified depending on what characters are used in the path
-            nessusDir = re.search("nessusDir=([0-9A-Za-z.-_/\"\']+)", config).group(1)
-            return(url, token, siteID, nessusDir)
-    except IOError as error:
-        raise error
-    except FileNotFoundError as error:
-        raise error
+                    python3 -m importNessus -u https://custom.runzero.com -s 92hu9740-1ed3-4365-b6a4-733638f2a63d -d /documents/scans/""")
 
 def importScan(uri, token, siteID, scan):
     """ Upload a .nessus scan file . 
@@ -104,50 +75,35 @@ if __name__ == "__main__":
     if "-h" in sys.argv:
         usage()
         exit()
-    if "-g" in sys.argv:
-        genConfig()
-    config = False
-    configFile = ''
-    consoleURL = 'https://console.runzero.com'
-    token = ''
-    siteID = ''
-    path = ''
-    if "-c" in sys.argv:
-        try:
-            config = True
-            configFile =sys.argv[sys.argv.index("-c") + 1]
-            confParams = readConfig(configFile)
-            consoleURL = confParams[0]
-            token = confParams[1]
-            siteID = confParams[2]
-            path = confParams[3]
-        except IndexError as error:
-            print("Config file switch used but no file provided!\n")
-            usage()
-            exit()
-    else:
+    consoleURL = os.environ["CONSOLE_BASE_URL"]
+    token = os.environ["RUNZERO_ORG_TOKEN"]
+    siteID = os.environ["SITE_ID"]
+    path = os.environ["NESSUS_DIR"]
+    if token == '':
         token = getpass(prompt="Enter your Organization API Key: ")
-    if "-s" in sys.argv and not config:
+    if "-s" in sys.argv:
         try:
             siteID = sys.argv[sys.argv.index("-s") + 1]
         except IndexError as error:
             print("site ID switch used but site ID not provided!\n")
             usage()
             exit()
-    if "-d" in sys.argv and not config:
+    if "-d" in sys.argv:
         try:
             path = sys.argv[sys.argv.index("-d") + 1]
         except IndexError as error:
             print("Directory switch used but directory not provided!\n")
             usage()
             exit()
-    if "-u" in sys.argv and not config:
+    if "-u" in sys.argv:
         try:
             consoleURL = sys.argv[sys.argv.index("-u") + 1]
         except IndexError as error:
             print("URI switch used but URI not provided!\n")
             usage()
             exit()
+    if consoleURL == '':
+         consoleURL = input('Enter the URL of the console (e.g. http://console.runzero.com): ')
     if siteID == '':
         siteID = input("Please provide the Site ID to apply the vulnerability scan data to: ")
     if path == '':
