0a8db086e4e4a2a08c6df32f7ee34702af591ed6
TechnoSavage <technosavage@protonmail.com>
2023-09-15 14:04:13 -0400

    .

---
+++ custom_integrations/Snipe-IT/README.md
@@ -0,0 +1,76 @@
+# Snipe-IT custom integrations for runZero
+Custom integration designed to pull asset information from Snipe-IT ITAM and create or merge to runZero asset data. Snipe-IT is highly customizable in terms of the field an asset can contain; some relevant attributes are hardware serial number, maufacturer, and hardware EOL. This integration is useful for representing assets that are part of the network infrastructure that do not communicate at OSI Layer 2 and above (e.g. network TAPs). 
+
+## Background
+
+Requirements (Global)
+
+- Python 3.8+
+- pipenv - https://pypi.org/project/pipenv/
+
+## runZero Custom Integration Docs
+
+- Inbound Integrations - https://www.runzero.com/docs/integrations-inbound/#custom-integrations
+- SDK Docs- https://runzeroinc.github.io/runzero-sdk-py/index.html
+
+## Snipe-IT API Docs
+
+- Snipe-IT API Reference: https://snipe-it.readme.io/reference/api-overview
+- Snipe-IT Docs: https://snipe-it.readme.io/docs
+
+## Configuration Explained
+
+- There is a sample environment variables file called .env_sample under the root folder of this project
+- You will clone this file and create one called .env where you actually input all of your secrets (API Keys + Access Key Pairs)
+- Parameters - explanations of what you see in .env_sample
+
+```
+RUNZERO_BASE_URL - The domain or IP of the runZero console, minus any resource path e.g. "https://console.runzero.com"
+RUNZERO_ACCOUNT_TOKEN - The account API token for the console instance. Found in the console under Account -> Account Settings -> Account API keys
+RUNZERO_CLIENT_ID - runZero API client ID. Created in the console under Account -> API clients
+RUNZERO_CLIENT_SECRET - Associated API client secret to the above ID. Displayed when creating an new API client
+RUNZERO_ORG_ID - UUID of the Organization to which integration will send data. Found in the console URL after navigating to Organizations and clicking organization name
+RUNZERO_SITE_NAME - Name of the site to which the integration will send data.  
+RUNZERO_SITE_ID - UUID of the Site to which integration will send data. Found in the console URL after navigating to Sites and clicking site name.
+RUNZERO_CUSTOM_SOURCE_ID - UUID of the Custom Integration Source. Found in the console URL after navigating to Accounts -> Custom Integrations and clicking the integration name.
+RUNZERO_IMPORT_TASK_NAME - Name of the Custom Integration Source. Created in the console under Accounts -> Custom Integrations.
+
+SNIPE_BASE_URL - The domain or IP of the Snipe-IT instance, minus any resource path e.g. "http://mycompany.snipeit"
+SNIPE_API_KEY - The API token for the Snipe instance. Created in user account dropdown -> Manage API Keys
+```
+
+## Getting Started
+
+- Clone this repository
+
+```
+git clone https://github.com/TechnoSavage/runZero.git
+```
+
+- Clone .env_sample to .env under the same directory
+
+```
+cp .env_sample .env
+```
+
+- Update all of the secrets in the `.env` needed based on the script you'd like to run (NOTE: you need to rerun pipenv shell anytime you update these  values to reload them)
+
+- Install dependancies
+
+```
+pipenv install -r requirements.txt
+```
+
+- Enter pipenv
+
+```
+pipenv shell
+```
+- In your runZero console, navigate to Account -> Custom Integrations -> Add custom integration. Name the integration consistent to the parameter RUNZERO_IMPORT_TASK_NAME in the .env file. Optionally upload an image for the source. 
+(This step is optional; the script will create the source if it doesn't already exist.)
+
+- run the script to sync assets. Sync can be scheduled with a job scheduler such as cron
+
+# Important
+
+For runZero to merge asset data from Snipe-IT to existing assets there must be a common matching asset attribute and field between what is reported by Snipe-IT and what can be found in runZero scans or passive discovery. A MAC address field in Snipe-IT is a recommended method. Snipe-IT has a default fieldset called 'Asset with MAC Address' that will serve this purpose. This script in its current form expects this field to be present for merge logic to work. The user is free to define and utilize other fields/attributes for this purpose as well (see SDK documentation).
---
+++ custom_integrations/Snipe-IT/snipeit.py
@@ -0,0 +1,170 @@
+# runZero Custom Integration with Snipe-IT
+# Docs: https://www.runzero.com/docs/integrations-inbound/
+# Docs: https://pypi.org/project/runzero-sdk/ 
+# Docs: https://snipe-it.readme.io/reference/api-overview
+# Docs: https://snipe-it.readme.io/docs
+# Prerequisite: pip install runzero-sdk
+
+import requests
+import os
+import uuid
+from flatten_json import flatten
+from ipaddress import ip_address
+from typing import Any, Dict, List
+import runzero
+from runzero.client import AuthError
+from runzero.api import CustomAssets, Sites
+from runzero.types import (CustomAttribute,ImportAsset,IPv4Address,IPv6Address,NetworkInterface,ImportTask)
+
+# Configure runZero variables
+# Script uses pipenv, but os.environ[] can be swapped out for a hardcoded value to make testing easier
+RUNZERO_BASE_URL = os.environ['RUNZERO_BASE_URL']
+RUNZERO_BASE_URL = f'{RUNZERO_BASE_URL}/api/v1.0'
+RUNZERO_CLIENT_ID = os.environ['RUNZERO_CLIENT_ID']
+RUNZERO_CLIENT_SECRET = os.environ['RUNZERO_CLIENT_SECRET']
+RUNZERO_ACCOUNT_TOKEN = os.environ['RUNZERO_ACCOUNT_TOKEN']
+RUNZERO_ORG_ID = os.environ['RUNZERO_ORG_ID']
+RUNZERO_CUSTOM_SOURCE_ID = os.environ['RUNZERO_CUSTOM_SOURCE_ID']
+RUNZERO_SITE_NAME = os.environ['RUNZERO_SITE_NAME']
+RUNZERO_SITE_ID = os.environ['RUNZERO_SITE_ID']
+RUNZERO_IMPORT_TASK_NAME = os.environ['RUNZERO_IMPORT_TASK_NAME']
+RUNZERO_HEADER = {'Authorization': f'Bearer {RUNZERO_ACCOUNT_TOKEN}'}
+
+# Configure Snipe-IT variables
+# Script uses pipenv, but os.environ[] can be swapped out for a hardcoded value to make testing easier
+SNIPE_BASE_URL = os.environ['SNIPE_BASE_URL']
+SNIPE_API_URL = f'{SNIPE_BASE_URL}/api/v1/hardware'
+SNIPE_API_KEY = os.environ['SNIPE_API_KEY']
+SNIPE_HEADERS = {'Accept': 'application/json',
+                 'Content-Type': 'application/json',
+                 'Authorization': f'Bearer {SNIPE_API_KEY}'}
+
+def build_assets_from_json(json_input: List[Dict[str, Any]]) -> List[ImportAsset]:
+    '''
+    This is an example function to highlight how to handle converting data from an API into the ImportAsset format that
+    is required for uploading to the runZero platform. This function assumes that the json has been converted into a list 
+    of dictionaries using `json.loads()` (or any similar functions).
+    '''
+
+    assets: List[ImportAsset] = []
+    for item in json_input:
+        # grab known API attributes from the json dict that are always present
+        id = item.get('id', uuid.uuid4)
+        flat_mac = flatten(item['custom_fields'])
+        mac = flat_mac["MAC Address_value"]
+        flat_model = flatten(item["model"])
+        device_type = flat_model["name"]
+        flat_man = flatten(item["manufacturer"])
+        manufacturer = flat_man["name"]
+
+        #  # if multiple mac addresses, take the first one
+        # if len(mac) > 0:
+        #    mac = mac[0].replace('-', ':')
+        # else:
+        #    mac = None
+
+        # create the network interface
+        network = build_network_interface(ips=[], mac=mac)
+
+        # *** Should not need to touch this ***
+        # handle any additional values and insert into custom_attrs
+        custom_attrs: Dict[str, CustomAttribute] = {}
+        for key, value in item.items():
+            if isinstance(value, dict):
+                for k, v in value.items():
+                    custom_attrs[k] = CustomAttribute(str(v)[:1023])
+            else:
+               custom_attrs[key] = CustomAttribute(str(value))
+
+        # Build assets for import
+        assets.append(
+            ImportAsset(
+                id=id,
+                network_interfaces=[network],
+                device_type=device_type,
+                manufacturer=manufacturer,
+                customAttributes=custom_attrs
+            )
+        )
+    return assets
+
+# *** Should not need to touch this ***
+def build_network_interface(ips: List[str], mac: str = None) -> NetworkInterface:
+    ''' 
+    This function converts a mac and a list of strings in either ipv4 or ipv6 format and creates a NetworkInterface that
+    is accepted in the ImportAsset
+    '''
+    ip4s: List[IPv4Address] = []
+    ip6s: List[IPv6Address] = []
+    for ip in ips[:99]:
+        ip_addr = ip_address(ip)
+        if ip_addr.version == 4:
+            ip4s.append(ip_addr)
+        elif ip_addr.version == 6:
+            ip6s.append(ip_addr)
+        else:
+            continue
+    if mac is None:
+        return NetworkInterface(ipv4Addresses=ip4s, ipv6Addresses=ip6s)
+    else:
+        return NetworkInterface(macAddress=mac, ipv4Addresses=ip4s, ipv6Addresses=ip6s)
+
+
+def import_data_to_runzero(assets: List[ImportAsset]):
+    '''
+    The code below gives an example of how to create a custom source and upload valid assets to a site using
+    the new custom source.
+    '''
+    # create the runzero client
+    c = runzero.Client()
+
+    # try to log in using OAuth credentials
+    try:
+        c.oauth_login(RUNZERO_CLIENT_ID, RUNZERO_CLIENT_SECRET)
+    except AuthError as e:
+        print(f'login failed: {e}')
+        return
+
+    # create the site manager to get our site information; set site ID for any new hosts
+    site_mgr = Sites(c)
+    site = site_mgr.get(RUNZERO_ORG_ID, RUNZERO_SITE_NAME)
+    if not site:
+        print(f'unable to find requested site')
+        return
+
+
+    # (Optional)
+    # Check for custom integration source in runZero and create new one if it doesn't exist
+    # You can create one manually within the UI and hardcode RUNZERO_CUSTOM_SOURCE_ID
+    '''
+    custom_source_mgr = CustomSourcesAdmin(c)
+    my_asset_source = custom_source_mgr.get(name='fortiedr')
+    if my_asset_source:
+        source_id = my_asset_source.id
+    else:
+        my_asset_source = custom_source_mgr.create(name='fortiedr')
+        source_id = my_asset_source.id
+    '''
+
+    # create the import manager to upload custom assets
+    import_mgr = CustomAssets(c)
+    import_task = import_mgr.upload_assets(org_id=RUNZERO_ORG_ID, site_id=RUNZERO_SITE_ID, custom_integration_id=RUNZERO_CUSTOM_SOURCE_ID, assets=assets, task_info=ImportTask(name=RUNZERO_IMPORT_TASK_NAME))
+
+    if import_task:
+        print(f'task created! view status here: {RUNZERO_BASE_URL}/tasks?task={import_task.id}')
+
+
+def main():
+    response = requests.get(SNIPE_API_URL, headers=SNIPE_HEADERS)
+    hardware_json_raw = response.json()
+    hardware_json = hardware_json_raw["rows"]
+
+    # Format asset list for import into runZero
+    import_assets = build_assets_from_json(hardware_json)
+
+    # Import assets into runZero
+    import_data_to_runzero(assets=import_assets)
+
+# *** Should not need to touch this ***
+if __name__ == '__main__':
+    main()
