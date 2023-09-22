bdb38df75b169dca5df1381020cd810bd9d46318
TechnoSavage <technosavage@protonmail.com>
2023-09-22 08:43:51 -0400

    better text output.

--- scripts/serialNumbers.py
+++ scripts/serialNumbers.py
@@ -1,5 +1,5 @@
 """ EXAMPLE PYTHON SCRIPT! NOT INTENDED FOR PRODUCTION USE! 
-    serialNumbers.py, version 3.0
+    serialNumbers.py, version 3.1
     Retrieve assets from console using Export API endpoint, extract defined fields and serial numbers,
     and, optionally, write to file. This allows users to pull assets and SN information with a predefined
     set of attributes included."""
@@ -23,7 +23,7 @@ def parseArgs():
     parser.add_argument('-p', '--path', help='Path to write file. This argument will take priority over the .env file', 
                         required=False, default=os.environ["SAVE_PATH"])
     parser.add_argument('-o', '--output', dest='output', help='output file format', choices=['txt', 'json', 'csv'], required=False)
-    parser.add_argument('--version', action='version', version='%(prog)s 3.0')
+    parser.add_argument('--version', action='version', version='%(prog)s 3.1')
     return parser.parse_args()
     
 def getAssets(url, token, filter=" ", fields=" "):
@@ -131,7 +131,7 @@ def main():
         fileName = f'{fileName}.txt'
         stringList = []
         for line in results:
-            stringList.append(str(line))
+            stringList.append(str(line).replace('{', '').replace('}', '').replace(': ', '='))
         textFile = '\n'.join(stringList)
         writeFile(fileName, textFile)
     elif args.output == 'csv':
@@ -139,7 +139,7 @@ def main():
         writeCSV(fileName, results)
     else:
         for line in results:
-            print(line)
+            print(json.dumps(line, indent=4))
     
 if __name__ == "__main__":
     main()
