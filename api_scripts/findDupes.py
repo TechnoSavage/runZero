c5f8641036d1e02f7cd361170888486eb979ca19
TechnoSavage <technosavage@protonmail.com>
2024-01-30 10:34:38 -0500

    corrected docstring

--- api_scripts/findDupes.py
+++ api_scripts/findDupes.py
@@ -32,7 +32,7 @@ def parseArgs():
 def getAssets(url, token, filter='', fields=''):
     """ Retrieve assets using supplied query filter from Console and restrict to fields supplied.
         
-        :param uri: A string, URI of runZero console.
+        :param url: A string, URL of runZero console.
         :param token: A string, Export API Key.
         :param filter: A string, query to filter returned assets(" " returns all).
         :param fields: A string, comma separated string of fields to return(" " returns all).
@@ -121,6 +121,7 @@ def writeDF(fileName, format, data):
     """ Write contents to output file. 
     
         :param filename: a string, name for file including.
+        :param format: a string, excel or csv
         :param contents: json data, file contents.
         :raises: IOError: if unable to write to file.  """
     
