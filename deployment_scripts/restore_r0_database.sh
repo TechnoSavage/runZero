c1e3252265fb7e9586fadbe2adb1d4cd208ea3d0
TechnoSavage <technosavage@protonmail.com>
2024-04-07 16:09:44 -0400

    .

---
+++ deployment_scripts/restore_r0_database.sh
@@ -0,0 +1,58 @@
+#!/bin/bash -x
+
+PROGNAME=$(basename $0)
+
+#Check for root privileges, exit if not run as root user
+if [[ $(id -u) != 0 ]]; then
+    cat << EOM
+
+This script must be run as root!
+Please rerun with sudo e.g.
+sudo ./explorer.sh
+
+EOM
+exit 1
+else
+    echo "Restoring backup..."
+fi
+
+usage () {
+  echo "$PROGNAME: Usage $PROGNAME -f  </path/to/filesystem_archive.tar.gz> -d </path/to/database_backup.sql.gz>
+  e.g. $PROGNAME -f runzero-backup-fs.tar.gz -d runzero.sql.gz
+  -f | --filesystem    Filesystem backup (tar.gz file)
+  -d | --database    PostgreSQL database backup (sql.gz file)"
+  return
+}
+
+filesystem=
+database=
+
+while [[ -n $1 ]]; do
+    case $1 in
+      -f | --filesystem)  shift
+                          filesystem=$( echo $1 )
+                          ;;
+      -d | --database)    shift
+                          database=$( echo $1 )
+                          ;;
+      -h | --help)        usage
+                          exit
+                          ;;
+      *)                  usage >&2
+                          exit 1
+                          ;;
+    esac
+    shift
+done
+
+if [[ ! -e $filesystem ]] || [[ ! -e $database ]]; then
+    echo "Filesystem backup or database backup not found!"
+    exit 1
+else
+    runzeroctl stop
+    tar -C -zxvf $filesystem
+    su - postgres
+    dropdb rumble; gzip -dc $database | psql
+    runzeroctl start
+    echo "Restore complete!"
+fi
