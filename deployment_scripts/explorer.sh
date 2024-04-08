ac537c317546b48c7d7c8d6721ddd5663f23c715
TechnoSavage <technosavage@protonmail.com>
2024-04-08 08:38:07 -0400

    .

--- deployment_scripts/explorer.sh
+++ deployment_scripts/explorer.sh
@@ -17,9 +17,40 @@ else
     echo "Beginning installation..."
 fi
 
+usage () {
+  echo "$PROGNAME: Usage $PROGNAME -t  <download token> -u <console URL> -b <binary architecture>
+  e.g. $PROGNAME -t xxxxxxxxxxxxxxx -c console.runzero.com -b runzero-explorer-linux-amd64
+  -t | --token      Download token of Organization
+  -u | --url        URL of runZero console
+  -b | --binary     Explorer binary architecture"
+  return
+}
+
 dltok=
 console='console.runzero.com'
 bin='runzero-explorer-linux-amd64'
+
+while [[ -n $1 ]]; do
+    case $1 in
+      -t | --token)  shift
+                          dltok=$( echo $1 )
+                          ;;
+      -u | --url)    shift
+                          console=$( echo $1 )
+                          ;;
+      -b | --binary) shift
+                          bin=$( echo $1 )
+                          ;;
+      -h | --help)        usage
+                          exit
+                          ;;
+      *)                  usage >&2
+                          exit 1
+                          ;;
+    esac
+    shift
+done
+
 curl -f -o runzero-explorer.bin "https://$console/download/explorer/$dltok/rzer0123/$bin"
 chmod + x runzero-explorer.bin
 ./runzero-explorer.bin
