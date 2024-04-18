52884dceb886cd516a9f06fd3229e6a11de1c394
TechnoSavage <technosavage@protonmail.com>
2024-04-18 18:54:13 -0400

    Fixed query that led to innacurate results. Now using pandas to write csv and excel output. Fixed formatting issue in csv and excel output.

--- api_scripts/serialNumbers.py
+++ api_scripts/serialNumbers.py
@@ -1,13 +1,13 @@
 """ EXAMPLE PYTHON SCRIPT! NOT INTENDED FOR PRODUCTION USE! 
-    serialNumbers.py, version 3.1
+    serialNumbers.py, version 3.2
     Retrieve assets from console using Export API endpoint, extract defined fields and serial numbers,
     and, optionally, write to file. This allows users to pull assets and SN information with a predefined
     set of attributes included."""
 
 import argparse
-import csv
 import json
 import os
+import pandas as pd
 import requests
 from datetime import datetime
 from flatten_json import flatten
@@ -22,14 +22,14 @@ def parseArgs():
                         nargs='?', const=None, required=False, default=os.environ["RUNZERO_EXPORT_TOKEN"])
     parser.add_argument('-p', '--path', help='Path to write file. This argument will take priority over the .env file', 
                         required=False, default=os.environ["SAVE_PATH"])
-    parser.add_argument('-o', '--output', dest='output', help='output file format', choices=['txt', 'json', 'csv'], required=False)
-    parser.add_argument('--version', action='version', version='%(prog)s 3.1')
+    parser.add_argument('-o', '--output', dest='output', help='output file format', choices=['txt', 'json', 'csv', 'excel'], required=False)
+    parser.add_argument('--version', action='version', version='%(prog)s 3.2')
     return parser.parse_args()
     
-def getAssets(url, token, filter=" ", fields=" "):
+def getAssets(url, token, filter='', fields=''):
     """ Retrieve assets using supplied query filter from Console and restrict to fields supplied.
         
-        :param uri: A string, URI of runZero console.
+        :param url: A string, URL of runZero console.
         :param token: A string, Export API Key.
         :param filter: A string, query to filter returned assets(" " returns all).
         :param fields: A string, comma separated string of fields to return(" " returns all).
@@ -72,29 +72,30 @@ def parseSNs(data):
                     root_keys_to_ignore.append(key)
                 flattened_items = flatten(nested_dict=item, root_keys_to_ignore=root_keys_to_ignore)
                 asset['hw.serialNumber'] = flattened_items.get('attributes_hw.serialNumber', '')
-                asset['snmp.serialNumbers'] = flattened_items.get('attributes_snmp.serialNumbers', '')
-                asset['ilo.serialNumber'] = flattened_items.get('attributes_ilo.serialNumber', '')
+                asset['snmp.serialNumbers'] = flattened_items.get('attributes_snmp.serialNumbers', '').split('\t')
+                asset['ilo.serialNumber'] = flattened_items.get('attributes_ilo.serialNumber', '').split('\t')
             assetList.append(asset)
         return(assetList)
     except TypeError as error:
         raise error
+    except AttributeError as error:
+        print("Data is not JSON object; make sure provided API key is correct")
+        exit()
     
-def writeCSV(fileName, contents):
-    """ Write contents to output file as CSV. 
+def writeDF(fileName, format, data):
+    """ Write contents to output file. 
     
         :param filename: a string, name for file including.
+        :param format: a string, excel or csv
         :param contents: json data, file contents.
-        :raises: IOError: if unable to write to file. """
+        :raises: IOError: if unable to write to file.  """
+    
+    df = pd.DataFrame(data)
     try:
-        with open(fileName, 'w') as o:
-            csv_writer = csv.writer(o)
-            count = 0
-            for item in contents:
-                if count == 0:
-                    header = item.keys()
-                    csv_writer.writerow(header)
-                    count += 1
-                csv_writer.writerow(item.values())
+        if format == "excel":
+            df.to_excel(f'{fileName}.xlsx')
+        else:
+            df.to_csv(f'{fileName}.csv', encoding='utf-8')
     except IOError as error:
         raise error
     
@@ -118,7 +119,7 @@ def main():
     if token == None:
         token = getpass(prompt="Enter your Export API Key: ")
     #Query to grab all assets with serial number fields
-    query = "protocol:snmp has:snmp.serialNumbers or hw.serialNumber:t or ilo.serialNumber:t"
+    query = "protocol:snmp or has:snmp.serialNumbers or hw.serialNumber:'%' or ilo.serialNumber:'%'"
     #fields to return in API call; modify for more or less
     fields = "id, hw, macs, attributes"
     assets = getAssets(args.consoleURL, token, query, fields)
@@ -133,9 +134,8 @@ def main():
             stringList.append(str(line).replace('{', '').replace('}', '').replace(': ', '='))
         textFile = '\n'.join(stringList)
         writeFile(fileName, textFile)
-    elif args.output == 'csv':
-        fileName = f'{fileName}.csv'
-        writeCSV(fileName, results)
+    elif args.output in ('csv', 'excel'):
+        writeDF(fileName, args.output, results)
     else:
         for line in results:
             print(json.dumps(line, indent=4))
