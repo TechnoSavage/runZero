6dbaa93de83ed494a3a7713c9eb88c245d2201e6
TechnoSavage <technosavage@protonmail.com>
2024-04-25 15:26:19 -0400

    .

---
+++ sdk-sample-scripts/Kismet/README.md
@@ -0,0 +1,88 @@
+# Kismet custom integration for runZero
+
+Custom integration designed to pull asset information from a specified data source in Kismet.
+## Background
+
+Requirements (Global)
+
+- Python 3.8+
+
+- pipenv - https://pypi.org/project/pipenv/
+
+## runZero Custom Integration Docs
+
+- Inbound Integrations - https://www.runzero.com/docs/integrations-inbound/#custom-integrations
+
+- SDK Docs- https://runzeroinc.github.io/runzero-sdk-py/index.html
+
+## Kismet API Docs
+
+- Kismet API Reference: https://www.kismetwireless.net/docs/api/rest_like/
+
+- Kismet Docs: https://www.kismetwireless.net/docs/readme/intro/kismet/
+
+## Configuration Explained
+
+- There is a sample environment variables file called .env_sample under the root folder of this project
+
+- You will clone this file and create one called .env where you actually input all of your secrets (API Keys + Access Key Pairs)
+
+- Parameters - explanations of what you see in .env_sample
+
+`RUNZERO_BASE_URL` - The domain or IP of the runZero console, minus any resource path e.g. https://console.runzero.com
+
+`RUNZERO_ACCOUNT_TOKEN` - The account API token for the console instance. Found in the console under Account -> Account Settings -> Account API keys
+
+`RUNZERO_CLIENT_ID` - runZero API client ID. Created in the console under Account -> API clients
+
+`RUNZERO_CLIENT_SECRET` - Associated API client secret to the above ID. Displayed when creating a new API client
+
+`RUNZERO_ORG_ID` - UUID of the Organization to which the integration will report data. Found in the console URL after navigating to "Organizations" and clicking on an organization name
+
+`RUNZERO_SITE_NAME` - Name of the site to which the integration will create new assets
+
+`RUNZERO_SITE_ID` - UUID of the Site to which the integration will create new assts. Found in the console URL after navigating to "Sites" and clicking on a site name
+
+`RUNZERO_CUSTOM_SOURCE_ID` - UUID of the Custom Integration Source. Found in the console URL after navigating to "Accounts" -> "Custom Integrations" and clicking on an  integration name
+
+`RUNZERO_IMPORT_TASK_NAME` - Name of the Custom Integration Source. Created in the console under "Accounts" -> "Custom Integrations"
+
+`KISMET_URL` - URL of Kismet webserver
+
+`KISMET_PORT` - Port of the Kismet webserver (default 2501)
+
+`KISMET_PHY` - UUID of the Kismet data source to fetch assets from
+
+`KISMET_KEY` - Kismet API Key (read-only)
+
+## Getting Started
+
+- Clone this repository
+
+```
+git clone https://github.com/TechnoSavage/runZero.git
+```
+
+- Clone runZero/sdk-sample-scripts/Kismet/.env_sample to .env under the same directory
+
+```
+cd runZero/sdk-sample-scripts/Kismet
+cp .env_sample .env
+```
+
+- Update all of the necessary secrets in the `.env` file (NOTE: you need to rerun pipenv shell anytime you update these  values to reload them)
+
+- Install dependancies
+
+```
+pipenv install -r requirements.txt
+```
+
+- Enter pipenv
+
+```
+pipenv shell
+```
+- In your runZero console, navigate to Account -> Custom Integrations -> Add custom integration. Name the integration consistent to the parameter RUNZERO_IMPORT_TASK_NAME in the .env file. Optionally upload an image for the source. 
+
+- run the script to sync assets. Sync can be scheduled with a job scheduler such as cron
---
+++ sdk-sample-scripts/Kismet/kismet.py
@@ -0,0 +1,159 @@
+# runZero Custom Integration with XYZ platform
+# Docs: https://www.runzero.com/docs/integrations-inbound/
+# Docs: https://pypi.org/project/runzero-sdk/ 
+# Docs: https://www.kismetwireless.net/docs/readme/intro/kismet/
+# Docs: https://www.kismetwireless.net/docs/api/rest_like/
+# Prerequisite: pip install runzero-sdk
+
+import json
+import requests
+import os
+import uuid
+from flatten_json import flatten
+from ipaddress import ip_address
+from typing import Any, Dict, List
+import runzero
+from runzero.client import AuthError
+from runzero.api import CustomAssets, Sites
+from runzero.types import (ImportAsset,IPv4Address,IPv6Address,NetworkInterface,ImportTask,Software,Vulnerability)
+
+# Configure runZero variables
+# Script uses pipenv, but os.environ[] can be swapped out for a hardcoded value to make testing easier
+RUNZERO_BASE_URL = os.environ['RUNZERO_BASE_URL']
+RUNZERO_BASE_URL = f'{RUNZERO_BASE_URL}/api/v1.0'
+RUNZERO_CLIENT_ID = os.environ['RUNZERO_CLIENT_ID']
+RUNZERO_CLIENT_SECRET = os.environ['RUNZERO_CLIENT_SECRET']
+RUNZERO_ORG_ID = os.environ['RUNZERO_ORG_ID']
+RUNZERO_CUSTOM_SOURCE_ID = os.environ['RUNZERO_CUSTOM_SOURCE_ID']
+RUNZERO_SITE_NAME = os.environ['RUNZERO_SITE_NAME']
+RUNZERO_SITE_ID = os.environ['RUNZERO_SITE_ID']
+RUNZERO_IMPORT_TASK_NAME = os.environ['RUNZERO_IMPORT_TASK_NAME']
+
+# Configure Integration API variables (examples provided)
+# Script uses pipenv, but os.environ[] can be swapped out for a hardcoded value to make testing easier
+KISMET_URL = os.environ['KISMET_URL']
+KISMET_PORT = os.environ['KISMET_PORT']
+KISMET_PHY = os.environ['KISMET_PHY']
+KISMET_KEY = os.environ['KISMET_KEY']
+
+def build_assets_from_json(json_input: List[Dict[str, Any]]) -> List[ImportAsset]:
+
+    assets: List[ImportAsset] = []
+    for item in json_input:
+        #Assign API attributes from the json dict that correspond to SDK
+        item = flatten(item)
+        asset_id = item.get('id', str(uuid.uuid4()))
+        mac = item.get('kismet.device.base.macaddr', None)
+        device_type = item.get('kismet.device.base.type')
+        manuf = item.get('kismet.device.base.manuf')
+        name = item.get('kismet.device.base.commonname')
+        first_seen = item.get('kismet.device.base.first.time')
+
+        # create the network interface
+        network = build_network_interface(ips=[], mac=mac)
+
+        # handle any additional values and insert into custom_attrs
+        custom_attrs: Dict[str] = {}
+        for key, value in item.items():
+            if isinstance(value, dict):
+                for k, v in value.items():
+                    custom_attrs[k] = str(v)[:1023]
+            else:
+               custom_attrs[key] = str(value)[:1023]
+
+        # Build assets for import
+        assets.append(
+            ImportAsset(
+                id=asset_id,
+                networkInterfaces=[network],
+                deviceType=device_type,
+                manufacturer=manuf,
+                hostnames=[name],
+                firstSeenTS=first_seen,
+                customAttributes=custom_attrs,
+            )
+        )
+    return assets
+
+def build_network_interface(ips: List[str], mac: str = None) -> NetworkInterface:
+    ''' 
+    This function converts a mac and a list of strings in either ipv4 or ipv6 format and creates a NetworkInterface that
+    is accepted in the ImportAsset
+    '''
+    ip4s: List[IPv4Address] = []
+    ip6s: List[IPv6Address] = []
+    for ip in ips[:99]:
+        ip_addr = ip_address(ip)
+        if ip_addr.version == 4:
+            ip4s.append(ip_addr)
+        elif ip_addr.version == 6:
+            ip6s.append(ip_addr)
+        else:
+            continue
+    if mac is None:
+        return NetworkInterface(ipv4Addresses=ip4s, ipv6Addresses=ip6s)
+    else:
+        return NetworkInterface(macAddress=mac, ipv4Addresses=ip4s, ipv6Addresses=ip6s)
+
+def import_data_to_runzero(assets: List[ImportAsset]):
+    '''
+    The code below gives an example of how to create a custom source and upload valid assets to a site using
+    the new custom source.
+    '''
+    # create the runzero client
+    c = runzero.Client()
+
+    # try to log in using OAuth credentials
+    try:
+        c.oauth_login(RUNZERO_CLIENT_ID, RUNZERO_CLIENT_SECRET)
+    except AuthError as e:
+        print(f'login failed: {e}')
+        return
+
+    # create the site manager to get our site information; set site ID for any new hosts
+    site_mgr = Sites(c)
+    site = site_mgr.get(RUNZERO_ORG_ID, RUNZERO_SITE_NAME)
+    if not site:
+        print(f'unable to find requested site')
+        return
+
+    # create the import manager to upload custom assets
+    import_mgr = CustomAssets(c)
+    import_task = import_mgr.upload_assets(org_id=RUNZERO_ORG_ID, site_id=RUNZERO_SITE_ID, custom_integration_id=RUNZERO_CUSTOM_SOURCE_ID, assets=assets, task_info=ImportTask(name=RUNZERO_IMPORT_TASK_NAME))
+
+    if import_task:
+        print(f'task created! view status here: {RUNZERO_BASE_URL}/tasks?task={import_task.id}')
+
+def get_kismet_assets(cookie=KISMET_KEY, uri=KISMET_URL, port=KISMET_PORT, phy=KISMET_PHY):
+    '''
+        Retrieve assets using supplied query filter from Console and restrict to fields supplied.
+            
+            :param cookie: A string, Kismet session cookie.
+            :param KISMET_URL: A string, URL of Kismet webserver.
+            :param KISMET_PORT: A string, Port of the Kismet webserver.
+            :param KISMET_PHY: A string, UUID of the Kismet data source.
+            :returns: A dict, Kismet asset data from given source.
+            :raises: ConnectionError: if unable to successfully make GET request to Kismet webserver.
+    '''
+    url = f"{uri}:{port}/devices/views/seenby-{phy}/devices.json"
+    headers = {'Cookie': f"KISMET={cookie}",
+               'Content-Type': 'application/json',
+               'Accept': 'application/json'}
+    try:
+        response = requests.get(url, headers=headers)
+        return json.loads(response.content)
+    except ConnectionError as error:
+        print("No Response from Kismet server.", error)
+        exit()
+
+def main():
+    asset_json = get_kismet_assets()
+
+    # Format asset list for import into runZero
+    import_assets = build_assets_from_json(asset_json)
+
+    # Import assets into runZero
+    import_data_to_runzero(assets=import_assets)
+
+if __name__ == '__main__':
+    main()
