d0c99f747d253fbac816aa089222dadf52a5756e
TechnoSavage <technosavage@protonmail.com>
2024-05-15 17:16:55 -0400

    .

--- cli_scanner_scripts/ot_full.sh
+++ cli_scanner_scripts/ot_full.sh
@@ -15,11 +15,11 @@ fi
 usage () {
   echo "$PROGNAME: Usage $PROGNAME -i  <target input list>
   e.g. $PROGNAME -i /path/to/my_scan_targets
-  -i | --input_list   Text file with list of targets to scan
-  -c | --communities  Additional SNMP community strings (comma separated, no spaces)
-  -m | --modbus  <basic | regular (default) | extended>
-  -s | --s7comm  <true | false (default)>
-  -d | --dnp3    <require | prefer | ignore (default)>"
+  -i | --input_list                                     Text file with list of targets to scan
+  -c | --communities                                    Additional SNMP community strings (comma separated, no spaces)
+  -m | --modbus  <basic | regular (default) | extended> Specify Modbus identification level
+  -s | --s7comm  <true | false (default)>               Request s7comm extended information
+  -d | --dnp3    <require | prefer | ignore (default)>" Specify dnp3 banner address discovery
   return
 }
 
@@ -29,6 +29,10 @@ modbus="regular"
 s7comm="false"
 dnp3="ignore"
 probes="layer2,syn,bacnet,dahua-dhip,dns,dtls,ike,ipmi,kerberos,knxnet,l2t,l2tp,lantronix,ldap,mssql,netbios,ntp,openvpn,pca,sip,ssdp,snmp,ssh,tftp,ubnt,vmware,webmin,modbus,ethernetip,s7comm,fins,dnp3"
+modbus_valid=( "basic" "regular" "default" )
+s7comm_valid=( "true" "false" )
+dnp3_valid=( "require" "prefer" "ignore" )
+
 
 while [[ -n $1 ]]; do
     case $1 in
@@ -39,13 +43,13 @@ while [[ -n $1 ]]; do
                           comms=$( echo $1 )
                           ;;
       -m | --modbus)      shift
-                          modbus=$( echo $1 )
+                          modbus=$( echo $1 | tr '[:upper:]' '[:lower:]' )
                           ;;
       -s | --s7comm)      shift
-                          s7comm=$( echo $1 )
+                          s7comm=$( echo $1 | tr '[:upper:]' '[:lower:]' )
                           ;;
       -d | --dnp3)        shift
-                          dnp3=$( echo $1 )
+                          dnp3=$( echo $1 | tr '[:upper:]' '[:lower:]' )
                           ;;
       -h | --help)        usage
                           exit
@@ -60,12 +64,28 @@ done
 # test that input target list exists and that is a regular file, exit if either condition in not true
 if [[ ! -e $input_list ]]; then
     echo "input list of targets does not exist; ensure target list exists and rerun script."
-    usage()
+    usage
     exit 1
 fi
 if [[ ! -f $input_list ]]; then
     echo "Specified input list is not a regular file."
-    usage()
+    usage
+    exit 1
+fi
+#test if values provided for OT protocol options are valid and normalize to lowercase
+if [[ ! " ${modbus_valid[*]} " =~ [[:space:]]${modbus}[[:space:]] ]]; then
+    echo "Provided modbus identification level is not a valid option ( basic, regular, or extended )."
+    usage
+    exit 1
+fi
+if [[ ! " ${s7comm_valid[*]} " =~ [[:space:]]${s7comm}[[:space:]] ]]; then
+    echo "Provided option for requesting s7comm extended information is invalid ( true or false )."
+    usage
+    exit 1
+fi
+if [[ ! " ${dnp3_valid[*]} " =~ [[:space:]]${dnp3}[[:space:]] ]]; then
+    echo "Provided dnp3 banner address discovery option is invalid ( require, prefer, or ignore )."
+    usage
     exit 1
 fi
 
