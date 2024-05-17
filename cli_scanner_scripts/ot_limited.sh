85cfb1f1efc90d18b782db1ad7aef5e87e7ccdbd
TechnoSavage <technosavage@protonmail.com>
2024-05-17 12:48:07 -0400

    .

--- cli_scanner_scripts/ot_limited.sh
+++ cli_scanner_scripts/ot_limited.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 
 PROGNAME=$(basename $0)
 
@@ -8,15 +8,15 @@ if [[ $(id -u) != 0 ]]; then
 
 This script must be run as root!
 Please rerun with sudo e.g.
-sudo ./$PROGNAME
+sudo ./${PROGNAME}
 
 EOM
 exit 1
 fi
 
 usage () {
-  echo "$PROGNAME usage: 
-  $PROGNAME -i  </path/to/scan_targets_file> [ -c <snmp,community,strings> ]
+  echo "${PROGNAME} usage: 
+  ${PROGNAME} -i  </path/to/scan_targets_file> [ -c <snmp,community,strings> ]
   -i | --input_list   Text file with list of targets to scan
   -c | --communities  Additional SNMP community strings (comma separated, no spaces)"
   return
@@ -30,7 +30,7 @@ probes="layer2,syn,netbios,ntp,snmp,tftp"
 while [[ -n $1 ]]; do
     case $1 in
       -i | --input_list)  shift
-                          input_list=$( echo $1 )
+                          input_list="$1"
                           ;;
       -c | --communities) shift
                           comms+=",$1"
@@ -46,7 +46,7 @@ while [[ -n $1 ]]; do
 done
 
 # test that input target list exists and that is a regular file, exit if either condition in not true
-if [[ ! -e $input_list ]] || [[ ! -f $input_list ]]; then
+if [[ ! -e ${input_list} ]] || [[ ! -f ${input_list} ]]; then
     echo " Provided input list of targets does not exist or is not a regular file."
     usage
     exit 1
@@ -55,9 +55,9 @@ fi
 echo "Preparing OT Limited scan..."
 
 # define parameters to reflect OT limited scan at https://help.runzero.com/docs/playbooks/scanning-ot-networks/#step-3a-create-an-ot-limited-scan-template 
-runzero -i $input_list --host-ping -r 300 --max-host-rate 20 --max-ttl 64 --max-group-size 2048 \
-  --tcp-ports $ports \
-  --probes $probes \
-  --snmp-comms $comms
+runzero -i ${input_list} --host-ping -r 300 --max-host-rate 20 --max-ttl 64 --max-group-size 2048 \
+  --tcp-ports ${ports} \
+  --probes ${probes} \
+  --snmp-comms ${comms}
 
 echo "Scan complete!"
