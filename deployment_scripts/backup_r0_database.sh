a1888fa14a65b30b01c9d6ea7a71c05ee5645974
TechnoSavage <technosavage@protonmail.com>
2024-07-15 12:58:47 -0400

    .

--- deployment_scripts/backup_r0_database.sh
+++ deployment_scripts/backup_r0_database.sh
@@ -17,6 +17,7 @@ else
 fi
 
 tar zcvf runzero-backup-fs.tar.gz /etc/runzero/ /opt/runzero/ /lib/systemd/system/runzero-console.service /etc/systemd/system/multi-user.target.wants/runzero-console.service /usr/bin/runzeroctl
+#depending on installation the postgresql path writable by the postgres user may be different e.g. (debian = /var/lib/postgresql, RH = /var/lib/pgsql)
 su -c "pg_dumpall -f /var/lib/postgresql/runzero.sql && gzip /var/lib/postgresql/runzero.sql" postgres
 mv /var/lib/postgresql/runzero.sql.gz $(pwd)
 
