99f442529da2193ad5acaf5ec109cdb94ade3a32
TechnoSavage <technosavage@protonmail.com>
2024-12-18 12:18:23 -0500

    Changed datetime format in filename, check HTTP response status code.

--- api_scripts/tallyNew.py
+++ api_scripts/tallyNew.py
@@ -1,5 +1,5 @@
 """ EXAMPLE PYTHON SCRIPT! NOT INTENDED FOR PRODUCTION USE! 
-    tallyNew.py, version 3.2
+    tallyNew.py, version 3.3
     Script to retrieve last 'N' completed tasks and tally new asset counts. """
 
 import argparse
@@ -7,7 +7,7 @@ import csv
 import json
 import os
 import requests
-from datetime import datetime
+from datetime import datetime, timezone
 from flatten_json import flatten
 from getpass import getpass
 from requests.exceptions import ConnectionError
@@ -23,21 +23,27 @@ def parseArgs():
     parser.add_argument('-p', '--path', help='Path to write file. This argument will take priority over the .env file', 
                         required=False, default=os.environ["SAVE_PATH"])
     parser.add_argument('-o', '--output', dest='output', help='Output file format', choices=['txt', 'json', 'csv'], required=False)
-    parser.add_argument('--version', action='version', version='%(prog)s 3.2')
+    parser.add_argument('--version', action='version', version='%(prog)s 3.3')
     return parser.parse_args()
 
 def getTasks(url, token): 
-    """ Retrieve Tasks from Organization corresponding to supplied token.
+    '''
+        Retrieve Tasks from Organization corresponding to supplied token.
 
-           :param token: A string, Account API Key.
-           :returns: A JSON object, runZero task data.
-           :raises: ConnectionError: if unable to successfully make GET request to console."""
+        :param token: A string, Account API Key.
+        :returns: A JSON object, runZero task data.
+        :raises: ConnectionError: if unable to successfully make GET request to console.
+    '''
+
     url = f"{url}/api/v1.0/org/tasks"
     payload = ""
     headers = {'Accept': 'application/json',
                'Authorization': f'Bearer {token}'}
     try:
         response = requests.get(url, headers=headers, data=payload)
+        if response.status_code != 200:
+            print('Unable to retrieve tasks' + response)
+            exit()
         content = response.content
         data = json.loads(content)
         return data
@@ -46,12 +52,15 @@ def getTasks(url, token):
         raise error
 
 def parseTasks(data, taskNo=1000):
-    """ Extract relevant fields from task data and tally new assets. 
+    '''
+        Extract relevant fields from task data and tally new assets. 
     
         :param data: JSON object, runZero task data.
         :param taskNo: an Integer, how many recent tasks to loop through.
         :returns: Dict Object, JSON formatted dictionary of relevant values.
-        :raises: TypeError: if data variable passed is not JSON format."""
+        :raises: TypeError: if data variable passed is not JSON format.
+    '''
+
     try:
         count = 0
         parsed = []
@@ -77,11 +86,14 @@ def parseTasks(data, taskNo=1000):
         raise error
     
 def writeCSV(fileName, contents):
-    """ Write contents to output file. 
+    '''
+        Write contents to output file. 
     
         :param filename: a string, name for file including.
         :param contents: json data, file contents.
-        :raises: IOError: if unable to write to file. """
+        :raises: IOError: if unable to write to file.
+    '''
+
     try:
         with open(fileName, 'w') as o:
             csv_writer = csv.writer(o)
@@ -96,11 +108,14 @@ def writeCSV(fileName, contents):
         raise error
 
 def writeFile(fileName, contents):
-    """ Write contents to output file. 
+    '''
+        Write contents to output file. 
     
         :param filename: a string, name for file including (optionally) file extension.
         :param contents: anything, file contents.
-        :raises: IOError: if unable to write to file. """
+        :raises: IOError: if unable to write to file.
+    '''
+
     try:
         with open( fileName, 'w') as o:
             o.write(contents)
@@ -110,7 +125,8 @@ def writeFile(fileName, contents):
 def main():
     args = parseArgs()
     #Output report name; default uses UTC time
-    fileName = f"{args.path}Asset_Tally_Report_{str(datetime.utcnow())}"
+    timestamp = str(datetime.now(timezone.utc).strftime('%y-%m-%d%Z_%H-%M-%S'))
+    fileName = f"{args.path}Asset_Tally_Report_{timestamp}"
     token = args.token
     if token == None:
         token = getpass(prompt="Enter your Organization API Key: ")
